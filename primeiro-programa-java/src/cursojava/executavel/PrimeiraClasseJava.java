3110cf659f776247b9b82138c0592c6c1b4637aa
Edvaldo <Edvaldo@Delarissia>
2022-08-06 10:44:57 -0300

    Operações lógicas aninhadas

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -3,49 +3,27 @@ package cursojava.executavel;
 public class PrimeiraClasseJava {
 	public static void main(String[] arg) {
 
-	int nota1 = 50;
-	int nota2 = 60;
-	int nota3 = 80;
+	int nota1 = 40;
+	int nota2 = 30;
+	int nota3 = 10;
 	int nota4 = 80;
 	int media = 0;		
 	
 	media = (nota1 + nota2 + nota3 + nota4) / 4;
 	
-	/* Condições lógica com IF e ELSE*/
+		/* Operações lógicas aninhadas: São operações dentro de operações */
 	
-	/*if (media >= 70) {
-		System.out.println("Aluno Aprovado!" +" "+ "Média: " + media );	
+	if (media >= 50) {
+		if (media >= 70) {
+			System.out.println("Aluno está aprovado");
+		} else {
+			System.out.println("Aluno está em recuperação");
+		}
+		
+	}else {
+		System.out.println("Aluno está reprovado");
 	}
-	else if (media >= 40 && media <= 69){
-		System.out.println("Aluno em Recuperação!" +" "+ "Média: " + media );
-	}
-	else { 
-		System.out.println("Aluno Reprovado!" +" "+ "Média: " + media);
-	}*/
 	
-	
-	/* Operadores ternários são para micro validações */
-	
-	/*String saidaResultado;
-	
-	saidaResultado = media >= 70 ? "Aluno Aprovado" : "Aluno Reprovado";
-	
-	System.out.println(saidaResultado);
-	*/
-	
-	/* Segunda Situação: com operadores ternários dentro de uma outra ternario
-	 * 
-	 * media >= 70 ? "Aluno Aprovado" - a primeira setença tem que ser verdadeira o que vem depois
-	 * ": " é a condição falsa ou neste caso abaixo seria uma outra condição e depois do segundo
-	 * " : " é falso.
-	 */
-	
-    String saidaResultado;
-	
-	saidaResultado = media >= 70 ? "Aluno Aprovado" : (media >= 50 && media <= 69) ? 
-			                  "Aluno em Recuperação" : "Aluno Reprovado";
-	
-	System.out.println(saidaResultado);
 	
 	
 }
